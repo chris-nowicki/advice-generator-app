8ef1f13725a97786164f75d1ee0cf8a5b5c06723
Chris Nowicki <chris@chrisnowicki.io>
2022-06-13 11:21:21 -0400

    almost final update with working site.  still need to update README

---
+++ script.js
@@ -0,0 +1,15 @@
+//function to get information from the Advice Slip API
+const adviceSlip = async () => {
+    //fetches the advice slip
+    let adviceSlip = await fetch("https://api.adviceslip.com/advice")
+    let adviceData =  await adviceSlip.json()
+
+    // set variables for the the API data
+    let slip = adviceData.slip
+    let adviceID = slip.id
+    let advice = slip.advice
+
+    // update the HTML with the advice
+    document.getElementById("advice-number").innerHTML = adviceID
+    document.getElementById("advice-text").innerHTML = advice
+}
